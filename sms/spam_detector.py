9468edde04956d49ac329acad88a38186651cf95
Carlos <carloslago@opendeusto.es>
2019-12-14 18:55:14 +0100

    Sms confusion fixed

--- sms/spam_detector.py
+++ sms/spam_detector.py
@@ -20,11 +20,6 @@ def train():
     test_X = np.load('dataset/processed/test_x.npy')
     test_Y = np.load('dataset/processed/test_y.npy')
 
-    print(X.shape)
-    print(Y.shape)
-    print(test_X.shape)
-    print(test_Y.shape)
-
     dropout_rate = 0.5
 
     input_shape = (MAXLEN,)
@@ -57,12 +52,11 @@ def train():
     )
     model.summary()
 
-    print(Y)
-
     history = model.fit(X, Y,
                         batch_size=BATCH_SIZE,
                         epochs=EPOCHS,
-                        validation_data=(test_X, test_Y)
+                        validation_data=(test_X, test_Y),
+                        verbose=2
                         )
 
     acc = history.history['accuracy']
@@ -96,24 +90,21 @@ def plot(model):
     test_Y = np.load('dataset/processed/test_y.npy')
 
     Y_pred = model.predict(np.array(test_X))
-    y_pred = np.around(Y_pred, 2)  # We divide because it is multilabel
+    y_pred = np.around(Y_pred)
 
     cm = multilabel_confusion_matrix(test_Y, y_pred)
     plt.figure()
 
-    plot_confusion_matrix(cm[0] + cm[1], figsize=(12, 8), hide_ticks=True)
+    plot_confusion_matrix(cm[0], figsize=(12, 8), hide_ticks=True)
     plt.xticks(range(2), ['SPAM', 'HAM'], fontsize=16)
     plt.yticks(range(2), ['SPAM', 'HAM'], fontsize=16)
-    # st.map(plt)
     plt.savefig("matrix.png")
     plt.show()
 
 
 if __name__ == '__main__':
     model = train()
-
     model.save('../streamlit_web/models/sms.h5')
     model = load_model("../streamlit_web/models/sms.h5",
                        custom_objects={"softmax_v2": tf.nn.softmax})
-
     plot(model)
